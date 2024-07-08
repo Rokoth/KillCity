505375e98d895e9a7aded156f241ae6234db1c36
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2024-07-08 20:35:25 +0500

    [Feature-0.0.1] Начало разработки игрового поля

---
+++ Server/Application/KillCity/KillCity/wwwroot/js/glscript.js
@@ -0,0 +1,94 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function start() {
+    var canvas = document.querySelector("#glcanvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        alert('у вас не работает webgl!')
+        return;
+    }
+
+    var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+
+    var positionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // три двумерных точки
+    var positions = [
+        10, 20,
+        80, 20,
+        10, 30,
+        10, 30,
+        80, 20,
+        80, 30,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // говорим использовать нашу программу (пару шейдеров)
+    gl.useProgram(program);
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    // Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 компоненты на итерацию
+    var type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
+    var normalize = false; // не нормализовать данные
+    var stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
+    var offset = 0;        // начинать с начала буфера
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+}
+
+
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);   // создание шейдера
+    gl.shaderSource(shader, source);      // устанавливаем шейдеру его программный код
+    gl.compileShader(shader);             // компилируем шейдер
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {                        // если компиляция прошла успешно - возвращаем шейдер
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+start();
